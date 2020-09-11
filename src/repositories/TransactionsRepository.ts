913ab77e8e514f5c9a621013cc2089e6061ea9f2
JoaoPGoes <joaopedro.goes13@gmail.com>
2020-09-11 20:06:34 -0300

    feat: create challenge features

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,30 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const data = await this.find();
+
+    const { income, outcome } = data.reduce(
+      (acumulador: Balance, elemento: Transaction) => {
+        if (elemento.type === 'income') {
+          // eslint-disable-next-line no-param-reassign
+          acumulador.income += Number(elemento.value);
+        } else {
+          // eslint-disable-next-line no-param-reassign
+          acumulador.outcome += Number(elemento.value);
+        }
+
+        return acumulador;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,65 @@
-// import AppError from '../errors/AppError';
+import { getRepository, getCustomRepository } from 'typeorm';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import AppError from '../errors/AppError';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
+
+    const { total } = await transactionRepository.getBalance();
+
+    if (type === 'outcome' && total < value)
+      throw new AppError('Unable to perform this transaction');
+
+    const categoryAlreadyExists = await categoryRepository.findOne({
+      where: { category },
+    });
+
+    if (categoryAlreadyExists) {
+      const categoryId = categoryAlreadyExists?.id;
+
+      const transaction = transactionRepository.create({
+        title,
+        type,
+        value,
+        category_id: categoryId,
+      });
+      await transactionRepository.save(transaction);
+
+      return transaction;
+    }
+
+    const newCategory = categoryRepository.create({
+      title: category,
+    });
+
+    await categoryRepository.save(newCategory);
+
+    const transaction = transactionRepository.create({
+      title,
+      type,
+      value,
+      category_id: newCategory.id,
+    });
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
